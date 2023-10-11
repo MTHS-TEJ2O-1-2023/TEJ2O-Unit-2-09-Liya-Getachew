879af428302c7ec88f1274c5c2456c8ceb97e115
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-10-11 17:26:44 +0000

    py version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,22 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Liya Getachew
+Created on: Oct 2023
+This module is a Micro:bit MicroPython program that plays rock paper scissors.
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HEART)
+
+# variables 
+rock_paper_scissors = -1
+score = 0
+
+while True:
+    if button_a.is_pressed():
+        score = score + 1
+    if button_b.is_pressed():
+        display.scroll("Score : " + str(score))
+
