5f242dee460ef2987922640f365fd783e7248069
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-10-12 23:55:38 +0000

    score check

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -19,6 +19,13 @@ rock = Image("00000:08880:08880:08880:00000")
 paper = Image("88888:80008:80008:80008:88888")
 
 while True:
+    if button_a.is_pressed():
+        score = score + 1
+        display.show(Image.YES)
+
+    if button_b.is_pressed():
+        display.scroll("Score : " + str(score))
+
     gesture = accelerometer.current_gesture()
     if gesture == "shake":
         # pick a number from 0 to 2
@@ -28,19 +35,17 @@ while True:
         # if rock_paper_scissor is 0
         if rock_paper_scissors == 0:
             display.show(scissor)
+            sleep(3000)
+            display.show(Image.HEART_SMALL)
 
         # if rock_paper_scissor is 1
         if rock_paper_scissors == 1:
             display.show(rock)
+            sleep(3000)
+            display.show(Image.HEART_SMALL)
 
         # if rock_paper_scissor is 2
         if rock_paper_scissors == 2:
             display.show(paper)
-
-        sleep(5000)
-        display.show(Image.HEART)
-
-    if button_a.is_pressed():
-        score = score + 1
-    if button_b.is_pressed():
-        display.scroll("Score : " + str(score))
+            sleep(3000)
+            display.show(Image.HEART_SMALL)
