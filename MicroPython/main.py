459eed91fcadc8d7c2c37f993dfb731a52d8e751
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-10-30 00:07:46 +0000

    format

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,8 +7,6 @@ This module is a Micro:bit MicroPython program that plays rock paper scissors.
 from microbit import *
 from random import *
 
-display.clear()
-display.show(Image.HEART)
 
 # variables
 rock_paper_scissors = -1
@@ -17,8 +15,12 @@ rock = Image("00000:08880:08880:08880:00000")
 paper = Image("88888:80008:80008:80008:88888")
 score = 0
 
+display.clear()
+display.show(Image.HEART)
+
 while True:
     if button_a.is_pressed():
+        # score counter
         score = score + 1
         display.show(Image.YES)
 
@@ -42,5 +44,6 @@ while True:
         # if rock_paper_scissor is 2
         if rock_paper_scissors == 2:
             display.show(paper)
-sleep(3000)
-display.show(Image.HEART_SMALL)
+
+        sleep(3000)
+        display.show(Image.HEART_SMALL)
